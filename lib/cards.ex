fb6594a3fb4dd0f543d3542f42f999cf6e82cb16
Thiago Leite Luiz <tlluiz@gmail.com>
2017-02-25 22:16:46 -0300

    Creates the methods contains? and deal to the Cards module.

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,9 +1,22 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diomonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def suffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 end
